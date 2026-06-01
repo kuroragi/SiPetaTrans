f4cfffac641c00c75f3e16d5c8e4f54326d3292f
kuromi <uum1612@gmail.com>
2026-06-01 20:29:46 +0700

    fix: perubahan seeder asset

--- database/seeders/AssetSeeder.php
+++ database/seeders/AssetSeeder.php
@@ -15,21 +15,19 @@ class AssetSeeder extends Seeder
     {
         $assets = [
             // Rambu Lalu Lintas (Type 1)
-            ['name' => 'Rambu Batas Kecepatan 40 km', 'asset_type_id' => 1, 'status' => 'baik', 'latitude' => -6.2088, 'longitude' => 106.8456, 'location' => 'Jl. Jend. Sudirman', 'quantity' => 3],
-            ['name' => 'Rambu Dilarang Parkir', 'asset_type_id' => 1, 'status' => 'perlu_perbaikan', 'latitude' => -6.2095, 'longitude' => 106.8470, 'location' => 'Jl. Gatot Subroto', 'quantity' => 5],
-            ['name' => 'Rambu One Way', 'asset_type_id' => 1, 'status' => 'rusak', 'latitude' => -6.2105, 'longitude' => 106.8480, 'location' => 'Jl. Imam Bonjol', 'quantity' => 2],
+            ['name' => 'Rambu Batas Kecepatan 40 km', 'asset_type_id' => 1, 'status' => 'baik', 'latitude' => -0.3127, 'longitude' => 100.3729, 'location' => 'Jl. Jend. Sudirman', 'quantity' => 3],
+            ['name' => 'Rambu Dilarang Parkir', 'asset_type_id' => 1, 'status' => 'perlu_perbaikan', 'latitude' => -0.3080, 'longitude' => 100.3707, 'location' => 'Jl. Haji Usmar Ismail', 'quantity' => 5],
+            ['name' => 'Rambu One Way', 'asset_type_id' => 1, 'status' => 'rusak', 'latitude' => -0.3030, 'longitude' => 100.369870, 'location' => 'Jl. Minangkabau', 'quantity' => 2],
             // Halte (Type 2)
-            ['name' => 'Halte Jom Gadang', 'asset_type_id' => 2, 'status' => 'baik', 'latitude' => -6.2120, 'longitude' => 106.8490, 'location' => 'Jl. Perwira', 'quantity' => 1],
-            ['name' => 'Halte Simpang Air Kuning', 'asset_type_id' => 2, 'status' => 'dalam_pemeliharaan', 'latitude' => -6.2130, 'longitude' => 106.8500, 'location' => 'Simpang Air Kuning', 'quantity' => 1],
+            ['name' => 'Halte Jam Gadang', 'asset_type_id' => 2, 'status' => 'baik', 'latitude' => -0.305597, 'longitude' => 100.369045, 'location' => 'Jl. Perwira', 'quantity' => 1],
+            ['name' => 'Halte Simpang Air Kuning', 'asset_type_id' => 2, 'status' => 'dalam_pemeliharaan', 'latitude' => -0.313749, 'longitude' => 100.384607, 'location' => 'Simpang Air Kuning', 'quantity' => 1],
             // APILL (Type 3)
-            ['name' => 'APILL Sudirman - Gatot', 'asset_type_id' => 3, 'status' => 'baik', 'latitude' => -6.2140, 'longitude' => 106.8510, 'location' => 'Persimpangan', 'quantity' => 1],
-            ['name' => 'APILL Jln Ahmad Yani', 'asset_type_id' => 3, 'status' => 'perlu_perbaikan', 'latitude' => -6.2150, 'longitude' => 106.8520, 'location' => 'Jl. Ahmad Yani', 'quantity' => 1],
+            ['name' => 'APILL Sudirman - Haji Usmar', 'asset_type_id' => 3, 'status' => 'baik', 'latitude' => -0.308088, 'longitude' => 100.370927, 'location' => 'Persimpangan', 'quantity' => 1],
             // Marka Jalan (Type 4)
-            ['name' => 'Marka Jalan Putih', 'asset_type_id' => 4, 'status' => 'baik', 'latitude' => -6.2160, 'longitude' => 106.8530, 'location' => 'Jl. Jend. Sudirman', 'quantity' => 500],
-            ['name' => 'Marka Jalan Kuning', 'asset_type_id' => 4, 'status' => 'rusak', 'latitude' => -6.2170, 'longitude' => 106.8540, 'location' => 'Jl. Veteran', 'quantity' => 200],
+            ['name' => 'Marka Jalan Putih', 'asset_type_id' => 4, 'status' => 'baik', 'latitude' => -0.292490, 'longitude' => 100.367020, 'location' => 'Jl. Veteran 84 - 68', 'quantity' => 500],
             // PAJ (Type 5)
-            ['name' => 'CCTV Sudirman 1', 'asset_type_id' => 5, 'status' => 'baik', 'latitude' => -6.2180, 'longitude' => 106.8550, 'location' => 'Jl. Jend. Sudirman', 'quantity' => 1],
-            ['name' => 'CCTV Gatot Subroto', 'asset_type_id' => 5, 'status' => 'baik', 'latitude' => -6.2190, 'longitude' => 106.8560, 'location' => 'Jl. Gatot Subroto', 'quantity' => 1],
+            ['name' => 'CCTV Lapangan Kantin', 'asset_type_id' => 5, 'status' => 'baik', 'latitude' => -0.312701, 'longitude' => 100.372928, 'location' => 'Jl. Jend. Sudirman', 'quantity' => 1],
+            ['name' => 'CCTV DPRD', 'asset_type_id' => 5, 'status' => 'baik', 'latitude' => -0.307266, 'longitude' => 100.369633, 'location' => 'Jl. Haji Usmar Ismail', 'quantity' => 1],
         ];
 
         foreach ($assets as $asset) {
